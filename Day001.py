086d1e4162e40f2bfd0e29d94a3cc852c47de4e0
sammy404421648 <54350846+sammy404421648@users.noreply.github.com>
2019-08-28 18:29:55 +0800

    Update Day001.py

--- Day001.py
+++ Day001.py
@@ -52,3 +52,77 @@ print("The Mean square error is %.3f" % (MSE))
 
 
 #%%
+
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+作業2：申論題目可直接將答案回覆在HW檔案裡面，Jupyter notebook可直接編輯文字。
+請上 Kaggle, 在 Competitions 或 Dataset 中找一組競賽或資料並寫下：
+
+資料:https://www.kaggle.com/c/titanic/overview
+主題:Titanic: Machine Learning from Disaster
+        
+1. 你選的這組資料為何重要
+
+預測哪些群組為在船難中最容易生存
+
+2. 資料從何而來 (tips: 譬如提供者是誰、以什麼方式蒐集)
+
+Kaggle competitions
+
+3. 蒐集而來的資料型態為何
+
+  結構化:PassengerId,Survived,Pclass,Age,SibSp,Parch,Fare
+非結構化:Name,Sex,Ticket,Cabin,Embarked
+註:
+PassengerId:乘客座號
+Survived:存活(1)、死亡(0)
+Pclass:社會地位
+Name:姓名
+Sex:性別
+Age:年齡
+SibSp:船上兄弟姐妹/配偶人數
+Parch:船上父母/小孩人數
+Ticket:船票編號
+Fare:票價
+Cabin:艙號
+Embarked:上船港口(C:Cherbourg,S:Southampton,Q:Queenstown)
+
+4. 這組資料想解決的問題如何評估
+
+此問題為分類問題(二分類)，可利用羅吉斯回歸
+
+
+# In[2]:
+
+
+作業3：申論題目可直接將答案回覆在HW檔案裡面，Jupyter notebook可直接編輯文字。
+
+想像你經營一個自由載客車隊，你希望能透過數據分析以提升業績，請你思考並描述你如何規劃整體
+的分析/解決方案：
+
+1. 核心問題為何 (tips：如何定義 「提升業績 & 你的假設」)
+
+定義:假設總收益超過上個月的10%，則認為提升業績
+
+例:假設上個月業績為NT$10000，令這月的業績為X
+
+Ho:X>=11000
+H1:X<11000
+
+2. 資料從何而來 (tips：哪些資料可能會對你想問的問題產生影響 & 資料如何蒐集)
+
+每當乘客下車時，請他掃QRCODE填問卷送折價卷，再由這些製成一份資料進行分析
+
+3. 蒐集而來的資料型態為何
+
+  結構化:車費、司機好感度、等車時間、年齡、收入
+非結構化:天氣(晴天、陰天、雨天)、性別、乘車目的(工作、遊玩)
+
+4. 你要回答的問題，其如何評估 (tips：你的假設如何驗證)
+
+車費為連續型，因此為回歸問題，先利用資料做回歸模型，再進行模型診斷，最後針對正係數最高的
+幾個做策略
